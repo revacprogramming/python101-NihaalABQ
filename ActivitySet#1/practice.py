ae3a41b30627487ea288a7b61d7a4a05f9c22d49
NihaalABQ <nihab777@gmail.com>
2022-07-08 09:03:44 +0530

    76

--- ActivitySet#1/practice.py
+++ ActivitySet#1/practice.py
@@ -1,4 +1,7 @@
-
-def print_pay():
-    print("nihaal")
-    
+$ pip install pandas
+import pandas as pd
+import matplotlib.pyplot as plt
+df= pd.read_csv("dataset0.csv")
+df.plot()
+df.plot(kind='scatter',x='x',y='y')
+df.plot(kind='density')
